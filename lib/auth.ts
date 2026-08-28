22e715608af82d66b75eea4b8424faac2973be4a
dreamcarss <dreamcarsvizag@gmail.com>
2026-08-28 07:25:00 +0530

    v1

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,6 +6,7 @@ import { prisma } from "@/lib/prisma";
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
+  trustHost: true,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
