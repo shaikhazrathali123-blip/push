6c62aa65fc53c4aab08d7306a511bbb57a7d3d7c
dreamcarss <dreamcarsvizag@gmail.com>
2026-08-28 07:27:42 +0530

    v1

--- next.config.js
+++ next.config.js
@@ -23,6 +23,7 @@ const withPWA = require("next-pwa")({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [{ protocol: "https", hostname: "lh3.googleusercontent.com" }],
   },
